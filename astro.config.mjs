58ad89083c8c053bdd174656eaede7a5cf30ed4d
Nayllen Soleika Rojas Martinez <nrojasma@LIM-T29KQPXG73.local>
2024-06-28 14:09:19 -0500

    ci: config deploy in vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://lsrv.pe",
   image: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com"]
   },
   devToolbar: {
     enabled: true
@@ -19,81 +21,83 @@ export default defineConfig({
     defaultLocale: "es",
     locales: ["es"],
     routing: {
-      prefixDefaultLocale: false,
-    },
+      prefixDefaultLocale: false
+    }
   },
   prefetch: true,
-  integrations: [
-    tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: "es", // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
-        locales: {
-          es: "es", // The `defaultLocale` value must present in `locales` keys
-        },
-      },
-    }),
-    starlight({
-      title: "Liga de Softbol RafaelVargas",
-      defaultLocale: "root",
+  integrations: [tailwind(), sitemap({
+    i18n: {
+      defaultLocale: "es",
+      // All urls that don't contain `fr` after `https://screwfast.uk/` will be treated as default locale, i.e. `en`
       locales: {
-        root: {
-          label: "Spanish",
-          lang: "es",
-        },
-        de: { label: "English", lang: "en" },
-        "zh-cn": { label: "简体中文", lang: "zh-CN" },
+        es: "es" // The `defaultLocale` value must present in `locales` keys
+      }
+    }
+  }), starlight({
+    title: "Liga de Softbol RafaelVargas",
+    defaultLocale: "root",
+    locales: {
+      root: {
+        label: "Spanish",
+        lang: "es"
       },
-      // https://starlight.astro.build/guides/sidebar/
-      sidebar: [
-        {
-          label: "Estadísticas",
-          autogenerate: { directory: "estadisticas" },
-        },
-        {
-          label: "Resultados",
-          autogenerate: { directory: "resultados" },
-        },
-        {
-          label: "Partidos",
-          autogenerate: { directory: "partidos" },
-        },
-      ],
-      social: {
-        facebook: "https://www.facebook.com/profile.php?id=61559654964803",
+      de: {
+        label: "English",
+        lang: "en"
       },
-      disable404Route: true,
-      customCss: ["./src/styles/starlight.css"],
-      favicon: "/favicon.ico",
-      components: {
-        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
-        Head: "./src/components/ui/starlight/Head.astro",
-      },
-      head: [
-        {
-          tag: "meta",
-          attrs: {
-            property: "og:image",
-            content: "https://lsrv.pe" + "/social.webp",
-          },
-        },
-        {
-          tag: "meta",
-          attrs: {
-            property: "facebook:image",
-            content: "https://screwfast.uk" + "/social.webp",
-          },
-        },
-      ],
-    }),
-    compressor({
-      gzip: false,
-      brotli: true,
-    }),
-  ],
+      "zh-cn": {
+        label: "简体中文",
+        lang: "zh-CN"
+      }
+    },
+    // https://starlight.astro.build/guides/sidebar/
+    sidebar: [{
+      label: "Estadísticas",
+      autogenerate: {
+        directory: "estadisticas"
+      }
+    }, {
+      label: "Resultados",
+      autogenerate: {
+        directory: "resultados"
+      }
+    }, {
+      label: "Partidos",
+      autogenerate: {
+        directory: "partidos"
+      }
+    }],
+    social: {
+      facebook: "https://www.facebook.com/profile.php?id=61559654964803"
+    },
+    disable404Route: true,
+    customCss: ["./src/styles/starlight.css"],
+    favicon: "/favicon.ico",
+    components: {
+      SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
+      Head: "./src/components/ui/starlight/Head.astro"
+    },
+    head: [{
+      tag: "meta",
+      attrs: {
+        property: "og:image",
+        content: "https://lsrv.pe" + "/social.webp"
+      }
+    }, {
+      tag: "meta",
+      attrs: {
+        property: "facebook:image",
+        content: "https://screwfast.uk" + "/social.webp"
+      }
+    }]
+  }), compressor({
+    gzip: false,
+    brotli: true
+  })],
   output: "static",
   experimental: {
     clientPrerender: true,
-    directRenderScript: true,
+    directRenderScript: true
   },
+  adapter: vercel()
 });
